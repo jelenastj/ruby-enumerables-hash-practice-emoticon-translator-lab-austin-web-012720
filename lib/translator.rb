05eaa2d5b93fa1a993f264a12dc4382d43b81c04
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-16 00:08:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,12 @@
 # require modules here
-
+require 'yaml'
 def load_library
+  YAML.load_file(loading a yaml file ruby)
   # code goes here
 end
+
+
+
 
 def get_japanese_emoticon
   # code goes here
