beac600e3014a71ea7c7e20ebea6e7d2bf51af91
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-16 01:42:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,11 +13,10 @@ def load_library(path)
 end
  
 
-
-
-
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (path, emoticon)
+  load_library(path)
+  
+ 
 end
 
 def get_english_meaning
