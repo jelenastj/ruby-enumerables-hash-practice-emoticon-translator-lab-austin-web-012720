42cf638624255f5f1b3e06dcc1b693711f147b74
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-16 01:47:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,16 +8,23 @@ def load_library(path)
   YAML.load_file(path).each do |meaning, symbol| 
   emoticons["get_meaning"][symbol[1]] = meaning
   emoticons["get_emoticon"][symbol[0]] = symbol[1]
-  end
+  end #eng to jap
   emoticons
 end
  
 
 def get_japanese_emoticon (path, emoticon)
   emoticons = load_library(path)
-  
+   result = emoticons["get_emoticon"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
  
 end
+
+
 
 def get_english_meaning
   # code goes here
