ae8dc75ea78256ae6a6010426cd15965a76b00cb
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-16 01:37:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(path)
   emoticons={"get_meaning"=>{}, "get_emoticon"=>{}}
   
   YAML.load_file(path).each do |meaning, symbol| 
-  emoticons["get_meaning"][symbol[1]] = meaning
+  emoticons["get_meaning"][emoticons[1]] = meaning
   emoticons["get_emoticon"][emoticons[0]] = emoticons[1]
   end
   emoticons
