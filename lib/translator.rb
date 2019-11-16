d2d7126a8fb238ae2cf41e05382acc5d3a7fbe21
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-16 01:21:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,9 @@
 # require modules here
 require 'yaml'
+
 def load_library(path)
-  new_hash={get_meaning=>{}, get_emoticon={}}
+  
+  new_hash={get_meaning=>{}, get_emoticon=>{}}
   
   #YAML.load_file(path)
   # code goes here
